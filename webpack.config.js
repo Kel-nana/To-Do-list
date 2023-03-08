325552385aa804f8dd60f067271576d5cb43a19f
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-03-08 12:24:14 +0300

    Fixing linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-     template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   module: {
@@ -24,6 +24,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
- 
-  mode:process.env.NODE_ENV === "production" ? "production" : "development"
+
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
